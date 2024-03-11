cba0809880c7478670b3e710c778979d25199e0a
Fahmad38 <fahmad38@gatech.edu>
2024-03-11 17:25:37 -0400

    added dynamic frame rate and resolution

--- src/generator.py
+++ src/generator.py
@@ -2,9 +2,14 @@ import subprocess
 import os
 import time
 import shutil
+from moviepy.editor import VideoFileClip
+
+inputName = "input_video.mp4"
+frameRate = 60
 
 
 def appendZero(i, length):
+
     string = str(i)
     while len(string) < length:
         string = "0" + string
@@ -19,10 +24,10 @@ def convert(string, length, increment, offset):
 
 
 def rename(directory, name, zLoc, increment, type):
+
     offset = 0
     if type == 'o':
         offset = -1
-
     directory = directory + "/"
     filenames = sorted(os.listdir(directory))
     filenames.pop(0)
@@ -39,15 +44,13 @@ def rename(directory, name, zLoc, increment, type):
 def generateVidFrames(input):
 
     input = "input/" + input
-
     ffmpeg = [
         'ffmpeg',
         '-i', input,
         "-vf",
-        'fps=12',
+        f'fps={frameRate // 2}',
         "vidFrames/frame_%06d.png"
     ]
-
     try:
         subprocess.run(ffmpeg, check=True)
         print('Suceeded')
@@ -63,11 +66,9 @@ def generateBlackFrames(h, w, num):
         'lavfi',
         '-i',
         'color=c=black:s=' + str(h) + 'x' + str(w),
-        '-vframes', str(num),
+        '-vframes', str(num // 2),
         "blackFrames/blackframe_%06d.png"
-
     ]
-
     try:
         subprocess.run(ffmpeg, check=True)
         print('Suceeded')
@@ -79,11 +80,8 @@ def moveAllFiles():
 
     sources = ["vidFrames/", "blackFrames/"]
     outLocation = "allFrames/"
-
     for source in sources:
-
         for filename in os.listdir(source):
-
             try:
                 if 'n' == filename[0]:
                     shutil.move(source + filename, outLocation)
@@ -94,18 +92,15 @@ def moveAllFiles():
 def compileVideo():
 
     ffmpeg = [
-
         "ffmpeg",
-        "-framerate", "30",
+        "-framerate", f"{frameRate}",
         "-i", "allFrames/nframes_%06d.png",
         "-c:v",
         "libx264",
         "-pix_fmt",
         "yuv420p",
         "output/output_video.mp4"
-
     ]
-
     try:
         subprocess.run(ffmpeg, check=True)
         print('Suceeded')
@@ -113,22 +108,58 @@ def compileVideo():
         print("Failed")
 
 
-generateBlackFrames(1640, 1624, 30)
+def resetFolders():
 
+    folder_path = 'allFrames'
+    if os.path.exists(folder_path):
+        print("path exists")
+        shutil.rmtree(folder_path)
+        os.makedirs(folder_path)
+    else:
+        print(f"The folder {folder_path} does not exist.")
+
+
+def findDuration():
+
+    video_path = f'input/{inputName}'
+    clip = VideoFileClip(video_path)
+    return clip.duration
+
+
+def findFrameRate():
+
+    video_path = f'output/output_video.mp4'
+    clip = VideoFileClip(video_path)
+    print(f'Frame Rate: {clip.fps}')
+
+
+def getHeight():
+
+    video_path = f'input/{inputName}'
+    video_clip = VideoFileClip(video_path)
+    height = video_clip.h  # Height of the video
+    video_clip.close()
+    return height
+
+
+def getWidth():
+
+    video_path = f'input/{inputName}'
+    video_clip = VideoFileClip(video_path)
+    width = video_clip.w  # Height of the video
+    video_clip.close()
+    return width
+
+
+generateBlackFrames(getHeight(), getWidth(), findDuration() * frameRate)
 time.sleep(2)
-
 rename("blackFrames", "nframes", 11, 2, "o")
-
 generateVidFrames("input_video.mp4")
-
 time.sleep(2)
-
 rename("vidFrames", "nframes", 6, 2, "e")
-
 time.sleep(1)
-
 moveAllFiles()
-
 time.sleep(1)
-
 compileVideo()
+resetFolders()
+findFrameRate()
